746dfbffc112976e2a9849b8e8faad37e2099ee8
Amine TIYAL <aminetiyal@gmail.com>
2020-09-09 16:15:36 +0100

    update serviceProvider

--- src/CategoriesServiceProvider.php
+++ src/CategoriesServiceProvider.php
@@ -11,33 +11,13 @@ class LaravelCategoriesServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-categories');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-categories');
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+        // $this->loadFactoriesFrom(__DIR__.'/../database/factories');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laravel-categories.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-categories'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-categories'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-categories'),
-            ], 'lang');*/
+                __DIR__.'/../config/config.php' => config_path('categories.php'),
+            ], 'categories');
 
             // Registering package commands.
             // $this->commands([]);
@@ -50,6 +30,6 @@ class LaravelCategoriesServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-categories');
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'categories');
     }
 }
