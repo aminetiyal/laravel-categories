35ce51c9bb83ff8030145ba77219e2b069fb19ea
Amine TIYAL <aminetiyal@gmail.com>
2020-09-12 14:12:25 +0100

    fix: config file

--- src/CategoriesServiceProvider.php
+++ src/CategoriesServiceProvider.php
@@ -41,6 +41,6 @@ class CategoriesServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'categories');
+        $this->mergeConfigFrom(__DIR__ . '/../config/categories.php', 'categories');
     }
 }
